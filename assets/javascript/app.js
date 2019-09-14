33bcf49bde1cd2f858a96bab6d640db6841668d7
Joshua Coleman <procoleman77@outlook.com>
2019-09-14 18:52:20 -0400

    Added js logic for images, added styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,22 @@
 
 // Create global variables array.
 var buttons = ['Cats', 'Food', 'Lions'];
+var API_KEY = 'nJ2X8ojY6X2AArANhiB28h1x5f335IIR&limit=25&rating=G';
+var endpoint = 'http://api.giphy.com/v1/gifs/search?api_key=nJ2X8ojY6X2AArANhiB28h1x5f335IIR&limit=25&rating=G';
 
 // Make ajax calls for buttons
 // create a on-click event for user
 // Debug along the way
 // Renders every button within the array or added
 function renderButtons() {
+    $('.recent-search').empty();
 
-    for(var i = 0; i < buttons.length; i++){
 
-     var buttonName = buttons[i];
+    for (var i = 0; i < buttons.length; i++) {
 
-     var button = `
+        var buttonName = buttons[i];
+
+        var button = `
      
      <div class="wrap-buttons">
      
@@ -26,7 +30,7 @@ function renderButtons() {
      
      `;
 
-     $('.recent-search').append(button);
+        $('.recent-search').append(button);
 
     }
 
@@ -34,6 +38,108 @@ function renderButtons() {
 
 renderButtons();
 
+// On click event to get new buttons from user
+
+$("#submit-button").on('click', function (event) {
+    event.preventDefault();
+
+
+    var value = $('#search').val();
+    // Checking
+
+
+    buttons.push(value);
+
+    renderButtons();
+})
+
+
+
+
+
+
+function searchGiphy(event) {
+    event.preventDefault();
+
+    var value = $('#search').val();
+    fetchGiphy(value);
+
+}
+// Looping over giphys
+function createGiphyTemplate(giphy){
+ var images =giphy.images[i];
+    var template = `
+    <div class="giphy">
+<i class="far fa-star favorite" data-id="${giphy.id}" data-star="false">
+</i>
+<div class="giphy-image">
+  <img src="${images.original_still.url}"
+   data-still="${images.original_still.url}" 
+   data-animate="${images.original.url}" 
+   data-state="still">
+  <i class="fa fa-play img-play"></i>
+</div>
+<div class="giphy-info">
+  <p>Rating: g</p>
+</div>
+
+<div class="giphy-footer" data-link="${giphy.embed_url}"> 
+  <p>Copy Link <i class="fa fa-link"></i></p>
+</div>
+</div>
+    
+`;
+
+return template;
+
+};
+
+
+function fetchGiphy(value) {
+    var url = endpoint + '&q=' + value;
+
+    $.ajax({
+        url: url,
+        method: "GET"
+
+    }).then(function (response) {
+        var giphys = response.data;
+        renderGiphys(giphys);
+        console.log(giphys);
+        // $(".giphy-content").append(p);
+        // $(".giphy-content").text(giphys);
+
+        // var results = response.data;
+
+        console.log("Value", value);
+
+
+
+    });
+
+
+
+};
+
+function renderGiphys(giphys) {
+
+    for (let i = 0; i < giphys.length; i++) {
+
+        var giphy = giphys[i];
+        var images =giphy.images;
+         var giphyTemp = createGiphyTemplate(giphy);
+     
+    };
+
+    $('.giphy-content').append(giphyTemp);
+
+
+}
+
+
+
+// Events
+$("#submit-button").on('click', searchGiphy);
 
 
 
@@ -67,9 +173,6 @@ renderButtons();
 
 
 
-
-// $(document).ready(function() {
-// created on click event
 // $("button").on("click", function () {
     // var person = $(this).attr("data-person");
     // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
